5a37981067866a56bc0c2932ad48e27b942bf60c
Subhanu Majumder <reach.subhanu@gmail.com>
2026-02-23 13:14:00 +0530

    feat(infra): finalize auth.py

--- app/core/auth.py
+++ app/core/auth.py
@@ -13,3 +13,11 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=settings.SECRET_KEY,
         lifetime_seconds=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+    )
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
