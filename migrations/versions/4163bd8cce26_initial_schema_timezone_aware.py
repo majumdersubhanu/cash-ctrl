f8eb47af2bd674339a2fdbef15211666ac0a900e
Subhanu Majumder <reach.subhanu@gmail.com>
2026-02-26 14:04:00 +0530

    feat(migrations): implement 4163bd8cce26_initial_schema_timezone_aware.py part 2

---
+++ migrations/versions/4163bd8cce26_initial_schema_timezone_aware.py
@@ -0,0 +1,15 @@
+"""Initial schema timezone aware
+
+Revision ID: 4163bd8cce26
+Revises: None
+Create Date: 2026-03-09 02:41:19.085998
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import fastapi_users_db_sqlalchemy
+
+
+# revision identifiers, used by Alembic.
