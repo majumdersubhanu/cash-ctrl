606b829e8534daf15597a9b10e42b967d986fb98
Subhanu Majumder <reach.subhanu@gmail.com>
2026-02-26 14:44:00 +0530

    feat(migrations): finalize env.py

--- migrations/env.py
+++ migrations/env.py
@@ -58,3 +58,17 @@ async def run_async_migrations() -> None:
     )
 
     async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
